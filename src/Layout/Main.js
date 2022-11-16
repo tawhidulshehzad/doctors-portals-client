416a7b4f00d33caa2605804f23c83d6986c3a341
shehzad1212 <tawhidulislam261@gmail.com>
2022-11-17 01:10:28 +0600

    all set to start

---
+++ src/Layout/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Home/Home/Shared/Footer/Footer";
+import Navbar from "../Pages/Home/Home/Shared/Navbar/Navbar";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
